fedc9056353f67a03076b2acc5daa1af120dd433
nicolas <nicolas.g.ferreira9@aluno.senai.br>
2026-03-30 11:37:27 -0300

    medias

---
+++ AULA 1/main3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+	float n1, n2, n3, resultado, jaguara;
+
+	printf("primeira nota: ");
+
+	scanf("%f", &n1);
+
+	printf("segunda nota: ");
+
+	scanf("%f", &n2);
+
+	printf("terceira nota:");
+
+	scanf("%f", &n3);
+
+	resultado=n1+n2+n3;
+	jaguara=resultado/3;
+	
+	if (jaguara >= 7.0) {
+		printf("%2.f = aprovado", jaguara);
+	}else if (jaguara <= 6.9){
+	    printf("%2.f = recuperação", jaguara);    
+	}else if (jaguara <= 2.9){
+	    printf ("%2.i = reprovado",jaguara);
+	}
+	
+	
+}

e6d3c66be451dab7ab7563c9b6574baf01d20d9c
nicolas <nicolas.g.ferreira9@aluno.senai.br>
2026-03-30 10:31:15 -0300

    exis2

---
+++ AULA 1/main2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int numero1;
+    int numero2;
+    int resultado;
+    printf("digite o primeiro numero:\n");
+    scanf("%i", &numero1);
+    
+    printf("digite o segundo: \n");
+    scanf("%i", &numero2);
+        
+        resultado=numero1 +numero2;
+          printf("%i + %i = %i",numero1, numero2, resultado);
+          
+}

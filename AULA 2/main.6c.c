c36d7db57e5c9b1518b38885363b978728a52e8c
nicolasgamafer-1 <nicolas.g.ferreira9@aluno.senai.br>
2026-04-06 09:09:51 -0300

    update1 main.6c.c

--- AULA 2/main.6c.c
+++ AULA 2/main.6c.c
@@ -8,7 +8,7 @@ int main() {
     printf("Digite um ano: ");
     scanf("%i", &ano);
 
-    if ((ano % 400 == 0)|(ano % 4 == 0 & ano % 100 != 0)) {
+    if ((ano % 400 == 0)||(ano % 4 == 0 & ano % 100 != 0)) {
         printf("%i é bissexto.", ano);
     } else {
         printf("%i não é bissexto.", ano);

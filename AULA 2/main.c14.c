6b5b337d5710ebe74472d139edde5c1b90a0fc7e
nicolas <nicolas.g.ferreira9@aluno.senai.br>
2026-04-06 10:18:37 -0300

    yuyu

---
+++ AULA 2/main.c14.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*14) Classificação de Nota com Conceito
+Leia uma nota de 0 a 10 e classifique:
+A (>= 9)
+B (7 – 8.9)
+C (5 – 6.9)
+D (< 5)*/
+int main() {
+    float nota;
+
+    printf("Digite a nota (0 a 10): ");
+    scanf("%f", &nota);
+
+    if (nota >= 9) {
+        printf("Conceito A");
+    } else if (nota >= 7) {
+        printf("Conceito B");
+    } else if (nota >= 5) {
+        printf("Conceito C");
+    } else {
+        printf("Conceito D");
+    }
+
+    
+}

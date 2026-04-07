b3126698803f1c3ed856c56cd918b713fc73667c
nicolas <nicolas.g.ferreira9@aluno.senai.br>
2026-04-07 08:20:19 -0300

    laço de repetiçao

---
+++ AULA 3/while.for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    for (int j=1; j <= 9; j=j+1){
+        printf("%i weramet end sama\n", j);
+    }
+    int x=5;
+    do{
+        printf("%i boa tarde\n", x);
+        x--;
+    } while(x>0);
+    
+}

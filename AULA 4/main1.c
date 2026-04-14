53ec42f4624efa9b347c52d206da7f12cc7d124d
nicolas <nicolas.g.ferreira9@aluno.senai.br>
2026-04-14 08:40:37 -0300

    ioioio

--- AULA 4/main1.c
+++ AULA 4/main1.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 /*1 - Um professor registrou as notas de 10 alunos em um array.
 Crie um programa que:
-calcule a média da turma
-identifique quantos alunos ficaram acima da média
-exiba apenas as notas acima da média*/
+calcule a mC)dia da turma
+identifique quantos alunos ficaram acima da mC)dia
+exiba apenas as notas acima da mC)dia*/
 int main() {
-    float nota[10], soma = 0, joce;
-    int i, media;
+	float nota[10], soma = 0, media;
+	int i, total=0, x=0;
 
-    for(i = 0; i < 10; i++) {
-        printf("DIGITE a nota dos alunos: ");
-        scanf("%f", &nota[i]);
-        soma = joce + nota[i];
-        media = joce * 0,10;
-    }
+	for(i = 0; i < 10; i++) {
+		printf("DIGITE a nota dos alunos: ");
+		scanf("%f", &nota[i]);
+		soma = soma + nota[i];
+		if (nota[i] >= 7){
+	    total++;
+		
+	}
+	}
+    media = soma * 0.10;
+	printf("media da turma: %.1f\n", media);
+	
 
-    printf("média: %.1f\n", soma);
-
-}
+	printf("total de alunos acima media: %i", total);
+	
+	while(x<10){
+	    if (nota[x] >= 7){
+	        printf("\n %i - nota: %.1f", x+1, nota[x]);
+	    }
+	    x++;
+	}
+	}
